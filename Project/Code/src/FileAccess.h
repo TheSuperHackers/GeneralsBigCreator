04644c872643db17880cb28a05ed6e53a3cf12a7
xezon <4720891+xezon@users.noreply.github.com>
2021-03-29 18:00:46 +0200

    Remove obsolete code from FileAccess.h

--- Project/Code/src/FileAccess.h
+++ Project/Code/src/FileAccess.h
@@ -16,23 +16,6 @@ namespace fileaccess
 		eError_WriteError,
 	};
 
-	enum EAccessMode
-	{
-		eAccessMode_Existence = 0,
-		eAccessMode_Write = 2,
-		eAccessMode_Read = 4,
-		eAccessMode_ReadAndWrite = 6,
-	};
-
-	enum EAccess
-	{
-		eAccess_Success = 0,
-		eAccess_Denied,
-		eAccess_NotFound,
-		eAccess_InvalidParameter,
-		eAccess_UnknownError,
-	};
-
 	typedef std::string TStringData;
 	typedef std::vector<char> TVectorData;
 
@@ -47,27 +30,4 @@ namespace fileaccess
 
 	EError WriteDataToStream(std::ostream& stream, const TStringData& data, size_t offset = 0xFFFFFFFFu);
 	EError WriteDataToStream(std::ostream& stream, const TVectorData& data, size_t offset = 0xFFFFFFFFu);
-
-	void GetHexString(const char* input, size_t len, std::string& output);
-
-	void ConvertToWinPath(wchar_t* filePath, const size_t len);
-	void ConvertToWinPath(std::wstring& filePath);
-
-	bool GetModuleDir(std::wstring& path);
-	bool GetSystemDir(std::wstring& path);
-	bool GetWorkingDir(std::wstring& path);
-
-	bool GetFileInModuleDir(const wchar_t* fileName, std::wstring& path);
-	bool GetFileInSystemDir(const wchar_t* fileName, std::wstring& path);
-	bool GetFileInWorkingDir(const wchar_t* fileName, std::wstring& path);
-
-	bool FileExists(const wchar_t* fileName);
-	bool FileWritable(const wchar_t* fileName);
-	bool FileReadable(const wchar_t* fileName);
-	EAccess GetFileAccess(const wchar_t* fileName, int accessMode);
-
-	int64 GetFileSize(const wchar_t* path);
-	int64 GetFileSize(std::fstream& file);
-
-	bool CreateRecursiveDirectory(const wchar_t* filePath, const int maxLevel);
 };
