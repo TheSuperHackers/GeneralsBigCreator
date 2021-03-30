160af2b8e33d040df247ed169db58798bacc8563
xezon <4720891+xezon@users.noreply.github.com>
2021-03-30 23:53:57 +0200

    Add MAKE_Release.bat file

---
+++ Project/MAKE_Release.bat
@@ -0,0 +1,11 @@
+set ArchiveName=GeneralsBigCreator_v1.1
+
+set ReleaseDir=Release
+set ReleaseUnpackedDir=ReleaseUnpacked
+
+if not exist %ReleaseDir% mkdir %ReleaseDir%
+if not exist %ReleaseUnpackedDir% mkdir %ReleaseUnpackedDir%
+
+xcopy /Y Code\vc9\Release\*.exe %ReleaseUnpackedDir%\
+
+tar.exe -a -c -C %ReleaseUnpackedDir% -f %ReleaseDir%\%ArchiveName%.zip *.*
